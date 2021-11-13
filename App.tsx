78a36121132c8ae5758a4ea9b8e73356cb408be9
Heesung Jang <heesungj7@gmail.com>
2021-11-14 04:13:10 +0900

    App Loading

--- App.tsx
+++ App.tsx
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// REACT
+import React, { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// REACT NATIVE
+import { StyleSheet, Text, View } from "react-native";
+
+// EXPO
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+
+// COMPONENTS
+import { StatusBar } from "expo-status-bar";
+import AppLoading from "expo-app-loading";
+
+interface Error {}
+
+const App: React.FC = () => {
+    const [loading, setLoading] = useState(true);
+
+    const onFinish = () => setLoading(false);
+
+    const preload = async () => {
+        const fontToLoad = [Ionicons.font];
+        const fontPromises = fontToLoad.map((font: any) =>
+            Font.loadAsync(font)
+        );
+        console.log(fontPromises);
+        await Promise.all(fontPromises);
+    };
+
+    if (loading) {
+        return (
+            <AppLoading
+                startAsync={preload}
+                onError={console.warn}
+                onFinish={onFinish}
+            />
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
