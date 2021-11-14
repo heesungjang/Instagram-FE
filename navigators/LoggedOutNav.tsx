15fa9642c880f306d11bdd8ee6048b9776b89f32
Heesung Jang <heesungj7@gmail.com>
2021-11-15 01:33:06 +0900

    created navigator

---
+++ navigators/LoggedOutNav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StackActions } from "@react-navigation/routers";
+import Welcome from "../screens/Welcome";
+import Login from "../screens/Login";
+import CreateAccount from "../screens/CreateAccount";
+
+const Stack = createStackNavigator();
+
+const LoggedOutNav: React.FC = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="welcome" component={Welcome} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="CreateAccount" component={CreateAccount} />
+        </Stack.Navigator>
+    );
+};
+
+export default LoggedOutNav;
