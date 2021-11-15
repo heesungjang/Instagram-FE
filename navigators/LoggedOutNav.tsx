7facea6147c68fd382b7e99543a5e35d06693b59
Heesung Jang <heesungj7@gmail.com>
2021-11-15 23:58:40 +0900

    refactor

--- navigators/LoggedOutNav.tsx
+++ navigators/LoggedOutNav.tsx
@@ -1,8 +1,16 @@
+// REACtT
 import React from "react";
+
+// NAVIGATION
 import { createStackNavigator } from "@react-navigation/stack";
-import Welcome from "../screens/Welcome";
+
+// SCREENS
 import Login from "../screens/Login";
+import Welcome from "../screens/Welcome";
 import CreateAccount from "../screens/CreateAccount";
+
+// TYPE
+// import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 const Stack = createStackNavigator();
 
