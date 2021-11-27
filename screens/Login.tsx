e92cc5aac6afa328010ed578c52903a57171dd12
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-11-27 23:59:48 +0900

    .

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -20,102 +20,102 @@ import { isLoggedInVar } from "../apollo";
 type IProps = NativeStackScreenProps<RootStackParamList, "Login">;
 
 type FormValues = {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 };
 
 const LOGIN_MUTATION = gql`
-    mutation login($username: String!, $password: String!) {
-        login(username: $username, password: $password) {
-            ok
-            token
-            error
-        }
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
     }
+  }
 `;
 
 const Login = ({ route: { params } }: IProps) => {
-    const { register, handleSubmit, setValue, watch } = useForm({
-        defaultValues: {
-            username: params?.username,
-            password: params?.password,
+  const { register, handleSubmit, setValue, watch } = useForm({
+    defaultValues: {
+      username: params?.username,
+      password: params?.password,
+    },
+  });
+
+  const onCompleted = (data: any) => {
+    const {
+      login: { ok, token },
+    } = data;
+    if (ok) {
+      isLoggedInVar(true);
+      console.log(token);
+    }
+  };
+
+  const [logInMutation, { loading }] = useMutation(LOGIN_MUTATION, { onCompleted });
+
+  const passwordRef = useRef(null);
+
+  const onValid: SubmitHandler<FormValues> = (data) => {
+    if (!loading) {
+      logInMutation({
+        variables: {
+          ...data,
         },
+      });
+    }
+  };
+
+  const onNext = (nextInput: React.RefObject<TextInput>) => {
+    nextInput?.current?.focus();
+  };
+
+  useEffect(() => {
+    register("username", {
+      required: true,
     });
+    register("password", {
+      // required: true,
+    });
+  }, [register]);
 
-    const onCompleted = (data: any) => {
-        const {
-            login: { ok, token },
-        } = data;
-        if (ok) {
-            isLoggedInVar(true);
-            console.log(token);
-        }
-    };
-
-    const [logInMutation, { loading }] = useMutation(LOGIN_MUTATION, { onCompleted });
-
-    const passwordRef = useRef(null);
-
-    const onValid: SubmitHandler<FormValues> = (data) => {
-        if (!loading) {
-            logInMutation({
-                variables: {
-                    ...data,
-                },
-            });
-        }
-    };
-
-    const onNext = (nextInput: React.RefObject<TextInput>) => {
-        nextInput?.current?.focus();
-    };
-
-    useEffect(() => {
-        register("username", {
-            required: true,
-        });
-        register("password", {
-            required: true,
-        });
-    }, [register]);
-
-    return (
-        <AuthLayout>
-            <KeyboardAvoidingView
-                style={{
-                    width: "100%",
-                }}
-                behavior="padding"
-                keyboardVerticalOffset={Platform.OS === "ios" ? 30 : 0}
-            >
-                <AuthTextInput
-                    value={watch("username")}
-                    placeholder="Username"
-                    returnKeyType="next"
-                    placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
-                    onSubmitEditing={() => onNext(passwordRef)}
-                    onChangeText={(text) => setValue("username", text)}
-                    autoCapitalize="none"
-                />
-                <AuthTextInput
-                    value={watch("password")}
-                    placeholder="Password"
-                    secureTextEntry
-                    returnKeyType="done"
-                    lastOne={true}
-                    placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
-                    onSubmitEditing={handleSubmit(onValid)}
-                    onChangeText={(text) => setValue("password", text)}
-                />
-                <AuthButton
-                    text="로그인"
-                    loading={loading}
-                    onPress={handleSubmit(onValid)}
-                    disabled={!watch("username") || !watch("password")}
-                />
-            </KeyboardAvoidingView>
-        </AuthLayout>
-    );
+  return (
+    <AuthLayout>
+      <KeyboardAvoidingView
+        style={{
+          width: "100%",
+        }}
+        behavior="padding"
+        keyboardVerticalOffset={Platform.OS === "ios" ? 30 : 0}
+      >
+        <AuthTextInput
+          value={watch("username")}
+          placeholder="Username"
+          returnKeyType="next"
+          placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+          onSubmitEditing={() => onNext(passwordRef)}
+          onChangeText={(text) => setValue("username", text)}
+          autoCapitalize="none"
+        />
+        <AuthTextInput
+          value={watch("password")}
+          placeholder="Password"
+          secureTextEntry
+          returnKeyType="done"
+          lastOne={true}
+          placeholderTextColor={"rgba(255, 255, 255, 0.6)"}
+          onSubmitEditing={handleSubmit(onValid)}
+          onChangeText={(text) => setValue("password", text)}
+        />
+        <AuthButton
+          text="로그인"
+          loading={loading}
+          onPress={handleSubmit(onValid)}
+          disabled={!watch("username") || !watch("password")}
+        />
+      </KeyboardAvoidingView>
+    </AuthLayout>
+  );
 };
 
 export default Login;
