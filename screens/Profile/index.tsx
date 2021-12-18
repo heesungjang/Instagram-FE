eb222e18cb11a7fbf71d7c5a3cac3018400b6ba1
Heesung Jang <heesungj7@gmail.com>
2021-12-18 22:43:31 +0900

    screens added

--- screens/Profile/index.tsx
+++ screens/Profile/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const Feed = () => {
+const Profile = () => {
   return (
     <View style={{ backgroundColor: "black", flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ color: "white", fontSize: 25 }}>hello, feed!</Text>
+      <Text style={{ color: "white", fontSize: 25 }}>hello, Profile!</Text>
     </View>
   );
 };
 
-export default Feed;
+export default Profile;
--- screens/Search/index.tsx
+++ screens/Search/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-const Feed = () => {
+const Search = () => {
   return (
     <View style={{ backgroundColor: "black", flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ color: "white", fontSize: 25 }}>hello, feed!</Text>
@@ -9,4 +9,4 @@ const Feed = () => {
   );
 };
 
-export default Feed;
+export default Search;
